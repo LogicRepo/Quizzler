d015119632d19af03f6239003692b5279f4227a8
avsheksingh <avsheksingh@outlook.com>
2019-04-09 16:05:48 +0530

    second commit

---
+++ app/src/main/java/com/example/avshek/quizzler/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.avshek.quizzler;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button mTrueButton;
+    Button mFalseButton;
+    TextView mQuestionTextVew;
+    TextView mScoreTextView;
+    ProgressBar mProgressBar;
+    int mIndex;
+    int mScore;
+    int mQuestion;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10,true),
+            new TrueFalse(R.string.question_11,false),
+            new TrueFalse(R.string.question_12,false),
+            new TrueFalse(R.string.question_13,true)
+    };
+
+    final int PROGRESS_BAR_INCREMENT = (int)Math.ceil(100) / mQuestionBank.length;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+
+        mQuestionTextVew = findViewById(R.id.question_text_view);
+
+        mScoreTextView = findViewById(R.id.score);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextVew.setText(mQuestion);
+
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion();
+                checkAnswer(false);
+            }
+        });
+
+        TrueFalse exampleQueston = new TrueFalse(R.string.question_1,true);
+
+        Random randomNumberGenerator = new Random();
+    }
+
+
+    private void updateQuestion(){
+        mIndex = (mIndex + 1) % mQuestionBank.length;
+
+        if(mIndex == 0){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Quizz Over");
+            alert.setCancelable(false);
+            alert.setMessage("You Scored " + mScore + " Points");
+            alert.setPositiveButton("Close Application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextVew.setText(mQuestion);
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+        mScoreTextView.setText("Score" + mScore + "/" + mQuestionBank.length);
+    }
+
+    private void checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[mIndex].isAnswer();
+
+        if(userSelection == correctAnswer){
+            Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            mScore = mScore + 1;
+        }else{
+            Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
